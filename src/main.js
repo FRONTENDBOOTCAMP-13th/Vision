efadeda39b9bcff36acfc71f59f6d3463af39e95
Stella97415 <stella97415@gmail.com>
2025-03-21 15:49:49 +0900

    모달 구현,멋사 로고 이미지 추가,index,main-js 파일 수정

--- src/main.js
+++ src/main.js
@@ -1,1 +1,6 @@
-import "./style.css"
+import './style.css';
+
+// 모달 닫기 기능
+document.getElementById('closeButton').addEventListener('click', function () {
+    document.getElementById('modalBackdrop').classList.add('hidden');
+});
